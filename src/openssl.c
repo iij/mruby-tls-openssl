be1c91f9a3d8286ac49f9e0863d1f509ddddadc0
Tomoyuki Sahara <tsahara@iij.ad.jp>
2014-07-14 20:11:18 +0900

    compile on OpenBSD.

--- src/openssl.c
+++ src/openssl.c
@@ -1,5 +1,8 @@
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <sys/time.h>
 #include <sys/wait.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
